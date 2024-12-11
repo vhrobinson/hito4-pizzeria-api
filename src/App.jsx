3f354283a07c1ec2d5eb9fa8f162a2407e80bc15
vhrobinson <vhrobinson@gmail.com>
2024-12-11 00:26:43 -0300

    feat(Pizza):se crea componente que muestra solo una pizza con id 001

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import Navbar from './components/Navbar'
 import Footer from './components/Footer'
-import Home from './pages/Home'
+import Pizza from './pages/components/Pizza'
+//import Home from './pages/Home'
 //import Cart from './pages/components/Cart'
 //import Home from './pages/Home'
 
@@ -10,7 +11,8 @@ function App() {
       <div className='flex flex-col min-h-screen'>
         <Navbar />
          <div className='flex-grow'>
-          <Home />
+        {/*}  <Home />*/}
+        <Pizza/>
         </div>  
         {/*<Cart/>*/}
         <Footer />
